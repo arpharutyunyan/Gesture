6d001a9fb9622f741a159e9301aeedb0ca10d6b5
arpharutyunyan <arpine.harutyunyana@gmai.com>
2021-06-06 15:50:16 +0400

    adding comment

--- gesture_recognition.py
+++ gesture_recognition.py
@@ -6,16 +6,16 @@ import tensorflow as tf
 
 from sklearn.model_selection import train_test_split
 
-EPOCHS = 10
-IMG_WIDTH = 30
-IMG_HEIGHT = 30
+EPOCHS = 3
+IMG_WIDTH = 640
+IMG_HEIGHT = 480
 NUM_CATEGORIES = 6
 TEST_SIZE = 0.4
 
 def main():
     # Check command-line arguments
     if len(sys.argv) not in [2, 3]:
-        sys.exit("Usage: python traffic.py data_directory [model.h5]")
+        sys.exit("Usage: python gesture_recognition.py data_directory [model.h5]")
 
     # Get image arrays and labels for all image files
     images, labels = load_data(sys.argv[1])
@@ -35,11 +35,11 @@ def main():
     # Evaluate neural network performance
     model.evaluate(x_test, y_test, verbose=2)
 
-    # Save model to file
-    if len(sys.argv) == 3:
-        filename = sys.argv[2]
-        model.save(filename)
-        print(f"Model saved to {filename}.")
+    # # Save model to file
+    # if len(sys.argv) == 3:
+    #     filename = sys.argv[2]
+    #     model.save(filename)
+    #     print(f"Model saved to {filename}.")
 
 
 def load_data(data_dir):
@@ -87,20 +87,29 @@ def get_model():
     `input_shape` of the first layer is `(IMG_WIDTH, IMG_HEIGHT, 3)`.
     The output layer should have `NUM_CATEGORIES` units, one for each category.
     """
-    model = tf.keras.models.Sequential([
+    # Create a convolutional neural network
+    model = tf.keras.models.Sequential(
+        [
+        # Convolutional layer. Learn 32 filters using a 3x3 kernel
         tf.keras.layers.Conv2D(
-            32, (3, 3), activation='relu', input_shape=(IMG_WIDTH, IMG_HEIGHT, 3)
+            680, (3, 3), activation='relu', input_shape=(IMG_WIDTH, IMG_HEIGHT, 3)
         ),
+        # Max-pooling layer, using 2x2 pool size
         tf.keras.layers.MaxPool2D(pool_size=(2, 2)),
         tf.keras.layers.Conv2D(
-            64, (3, 3), activation='relu', input_shape=((IMG_WIDTH)/2, (IMG_HEIGHT)/2, 3)
+            1200, (3, 3), activation='relu', input_shape=((IMG_WIDTH)/2, (IMG_HEIGHT)/2, 3)
         ),
         tf.keras.layers.MaxPool2D(pool_size=(2, 2)),
+
         tf.keras.layers.Flatten(),
-        tf.keras.layers.Dense(128, activation='relu'),
+        # Add a hidden layer with dropout
+        tf.keras.layers.Dense(2000, activation='relu'),
         tf.keras.layers.Dropout(0.3),
+        # Add an output layer with output units for all 6 gestures
         tf.keras.layers.Dense(NUM_CATEGORIES, activation='softmax')
     ])
+
+    # Train neural network
     model.compile(
         optimizer='adam',
         loss="categorical_crossentropy",
